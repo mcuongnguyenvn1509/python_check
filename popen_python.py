8ea98bef9d540a71fb865357a93e0d2caae704cb
mcuongnguyenvn1509 <44352679+mcuongnguyenvn1509@users.noreply.github.com>
2019-10-07 17:19:34 +0700

    Update popen_python.py

--- popen_python.py
+++ popen_python.py
@@ -1,1 +1,10 @@
+import subprocess
+import shlex
+argshlex=shlex.split('grep -rlw "Generated file" --include=*.{h,cpp}')
 
+print (argshlex)
+
+grep_process=subprocess.Popen(["grep", "-rlw", "Generated file", "--include=*.{h,cpp}"],stdout=subprocess.PIPE)
+cat_process=subprocess.Popen(["cat"], stdin=grep_process.stdout, stdout=subprocess.PIPE)
+output= cat_process.communicate()[0]
+print(output.decode())
