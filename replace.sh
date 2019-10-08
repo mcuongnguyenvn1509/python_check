5c50763ac7548ebf3c18e26eba548caebb0909c2
mcuongnguyenvn1509 <44352679+mcuongnguyenvn1509@users.noreply.github.com>
2019-10-08 10:13:50 +0700

    New improvement

--- replace.sh
+++ replace.sh
@@ -1,20 +1,18 @@
-#!/bin/bash
-DIR="$1"
+ #!/bin/bash
+ [ $# -eq 0 ] && { echo "Usage: $0 PATH_TO_FOLDER"; exit 1; }
+ arg="$1"
 
-file=domains.txt
-
-# set the Internal Field Separator to |
+ file=./domain.txt
+ # set the Internal Field Separator to |
  IFS='|'
- while read -r line ID new solution
+ while read -r ID original new
  do
-         [[ "$line" =~ ^#.*$ ]] && continue
+         [[ "$ID" =~ ^#.*$ ]] && continue
          ID=$(echo $ID | sed 's/^[ \t]*//;s/[ \t]*$//')
          printf "***Reading %s to repalce the source code...\n" $ID
+         original=$(echo $original | sed 's/^[ \t]*//;s/[ \t]*$//')
          new=$(echo $new | sed 's/^[ \t]*//;s/[ \t]*$//')
-         solution=$(echo $solution | sed 's/^[ \t]*//;s/[ \t]*$//')
-         printf "\"%s\" replacing by \"%s\" ...\n" $new $solution
+         printf "\"%s\" is being replaced by \"%s\" ...\n" $original $new
          #find $arg -type f | xargs sed -i "s/\b${original}\b/${new}/gi" ;
-	 grep -rwl "Generated file" $DIR --include=*.{cpp,h}| xargs sed -i "s/\b${original}\b/${new}/gi" ;
- 	 done < "$file"
-
-
+         grep -rn "Generated file" $arg --include=*.{h,cpp} | xargs sed -i "s/\b${original}\b/${new}/gi" ;
+ done < "$file"
