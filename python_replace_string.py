c73a3e1c67ac4c842027b1f597ad726fac196e33
mcuongnguyenvn1509 <44352679+mcuongnguyenvn1509@users.noreply.github.com>
2019-10-08 11:03:35 +0700

    Update python_replace_string.py

--- python_replace_string.py
+++ python_replace_string.py
@@ -1,6 +1,12 @@
-import os
-import subprocess
-arg="./"
-print ("start replacement")
-subprocess.call("replace.sh '%s'"% arg)
-print ("end replacement")
+ import os
+ import subprocess
+
+ arg="/home/ucu1hc/Downloads/Workspace_201909xx/test1/_data_to_replace"
+ #print ("start replacement")
+ #subprocess.call("replace.sh '%s'"% arg)
+ subprocess.call(['chmod', '-R', '777', 'replace.sh'])
+ print '%s'%str(arg)
+ subprocess.call(['sh','./replace.sh',str(arg)])
+ #output = subprocess.Popen(['bash','-c','./replace.sh',arg],shell=True,stderr=subprocess.PIPE)
+ #print output.communicate()
+ #print ("end replacement")
